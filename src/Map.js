18f53eeb4bed6cea298697b65820d5ae31148f4d
cormacgartland <cormac.gartland@gmail.com>
2021-10-12 23:49:36 -0400

    multiple markers mapped according to JSON data

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-import MapGl from 'react-map-gl'
+import React, { useState } from 'react';
+import MapGl, { Marker } from 'react-map-gl';
+import RoomIcon from '@material-ui/icons/Room.js';
+import * as truckData from './TruckData.json'
 
 const MAP_TOKEN = 'pk.eyJ1IjoiY29ybWFjZ2FydGxhbmQiLCJhIjoiY2t1ZjBxYTk5MXB2aDJ1bzhvOXA4dHAybyJ9.-_ZlY_g0JfjDIMiJmQomeA'
 
@@ -9,18 +11,31 @@ export default function Map() {
         latitude: 40.440624,
         longitude: -79.995888,
         zoom: 13,
+        // width: window.innerWidth,
+        height: window.innerHeight,
     });
 
     return (
         <div>
             <MapGl 
+                width="75vw"
                 {...viewPort}
-                width="100vw"
-                height="100vh"
-                mapStyle="mapbox://styles/mapbox/dark-v9"
+                mapStyle="mapbox://styles/mapbox/streets-v8"
                 onViewportChange={setViewPort}
-                mapboxApiAccessToken={MAP_TOKEN}
-            />
+                mapboxApiAccessToken={MAP_TOKEN}>
+                <div>
+                    {truckData.features.map((truckData, index) => (
+                        <Marker key={index}
+                            latitude={truckData.lat} 
+                            longitude={truckData.long} 
+                            offset={(-viewPort.zoom * 3) / 2} 
+                            offsetTop={-5}>
+                                <RoomIcon>
+                                </RoomIcon>
+                        </Marker>
+                    ))}
+                </div>
+            </MapGl>
         </div>
     )
 }
